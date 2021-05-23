2b1402aebdc2224aedfe55ec22af322d0b33439a
aberimen <berimen63@gmail.com>
2021-05-24 00:29:14 +0300

    GenericResponse for APIs

---
+++ hrms/src/main/java/com/aberimen/hrms/utils/GenericResponse.java
@@ -0,0 +1,12 @@
+package com.aberimen.hrms.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GenericResponse {
+	
+	private String message;
+
+}
