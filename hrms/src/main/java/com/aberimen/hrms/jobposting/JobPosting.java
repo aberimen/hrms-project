0b5f4fa7197132aca3cc28e52b4cd6739e226437
aberimen <berimen63@gmail.com>
2021-05-28 21:05:16 +0300

    JobPosting Entity created.

---
+++ hrms/src/main/java/com/aberimen/hrms/jobposting/JobPosting.java
@@ -0,0 +1,43 @@
+package com.aberimen.hrms.jobposting;
+
+import java.time.LocalDate;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import com.aberimen.hrms.jobposition.JobPosition;
+import com.aberimen.hrms.location.Location;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class JobPosting {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Basic(optional = false)
+	private String jobDescription;
+	
+	private int minSalary;
+	
+	private int maxSalary;
+	
+	@Basic(optional = false)
+	private int openPositions;
+	
+	private LocalDate deadline;
+	
+	@OneToOne
+	private Location location;
+	
+	@OneToOne(optional = false)
+	private JobPosition jobPosition; 
+
+	
+}
