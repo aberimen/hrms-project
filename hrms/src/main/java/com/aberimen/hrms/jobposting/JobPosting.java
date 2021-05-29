64f1e07e69c0b7fd3004d1f697b1654322656e95
aberimen <berimen63@gmail.com>
2021-05-29 09:02:22 +0300

    Add new fields

--- hrms/src/main/java/com/aberimen/hrms/jobposting/JobPosting.java
+++ hrms/src/main/java/com/aberimen/hrms/jobposting/JobPosting.java
@@ -9,37 +9,48 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
+
+import com.aberimen.hrms.employer.Employer;
 import com.aberimen.hrms.jobposition.JobPosition;
 import com.aberimen.hrms.location.Location;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobPosting {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@Lob
 	@Basic(optional = false)
 	private String jobDescription;
-	
+
 	private int minSalary;
-	
+
 	private int maxSalary;
-	
+
 	@Basic(optional = false)
 	private int openPositions;
-	
+
 	private LocalDate deadline;
-	
+
 	@OneToOne
 	private Location location;
-	
-	@OneToOne(optional = false)
-	private JobPosition jobPosition; 
 
-	
+	@OneToOne(optional = false)
+	private JobPosition jobPosition;
+
+	@OneToOne
+	private Employer employer;
+
+	private LocalDate postedAt;
+
+	boolean active;
 }
