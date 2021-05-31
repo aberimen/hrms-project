215d40cc63aa89faf6315b3943892c19481055ed
aberimen <berimen63@gmail.com>
2021-05-31 07:47:00 +0300

    Add verifyEmail

--- hrms/src/main/java/com/aberimen/hrms/user/UserService.java
+++ hrms/src/main/java/com/aberimen/hrms/user/UserService.java
@@ -6,31 +6,40 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-	
+
 	private UserRepository userRepository;
 
 	public UserService(UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
 	}
-	
-	
+
 	public void save(User user) {
-		
+
 		userRepository.save(user);
 	}
 
-
 	public User getUserByEmail(String email) {
 		Optional<User> inDB = userRepository.findByEmail(email);
-		
-		if(inDB.isPresent()) {
+
+		if (inDB.isPresent()) {
 			return inDB.get();
 		}
 		return null;
 	}
-	
-	
-	
+
+	public String verifyEmail(String email) {
+		Optional<User> inDBOptional = userRepository.findByEmail(email);
+
+		if (inDBOptional.isPresent()) {
+			User inDB = inDBOptional.get();
+			inDB.setEnabled(true);
+			userRepository.save(inDB);
+
+			return "email doğrulandı";
+		}
+
+		return "email doğrulanamadı";
+	}
 
 }
