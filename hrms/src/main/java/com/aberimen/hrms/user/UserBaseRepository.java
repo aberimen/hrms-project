5b12028c64ce58410e712f76523c0cf21c214bb9
aberimen <berimen63@gmail.com>
2021-05-31 07:31:46 +0300

    Create UserBaseRepository

---
+++ hrms/src/main/java/com/aberimen/hrms/user/UserBaseRepository.java
@@ -0,0 +1,13 @@
+package com.aberimen.hrms.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface UserBaseRepository<T extends User> extends JpaRepository<T, Long>{
+
+	Optional<T> findByEmail(String email);	
+
+}
