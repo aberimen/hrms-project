a5a5a6a0fa13a822be86d3a4e8335f645989a7a0
aberimen <berimen63@gmail.com>
2021-06-02 16:30:24 +0300

    Create Experience Entity for relationship to Candidate Resume

---
+++ hrms/src/main/java/com/aberimen/hrms/experience/Experience.java
@@ -0,0 +1,40 @@
+package com.aberimen.hrms.experience;
+
+import java.time.LocalDate;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.aberimen.hrms.jobposition.JobPosition;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Experience {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Basic(optional = false)
+	private String companyName;
+	
+	@Basic(optional = false)
+	private boolean currentJob;
+	
+	@OneToOne(optional = false)
+	private JobPosition jobPosition;
+	
+	@Basic(optional = false)
+	private LocalDate startDate;
+	
+	private LocalDate endDate;
+	
+	
+	
+}
