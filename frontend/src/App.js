e0819f87eb0397dd9f486225b9241a54103ccf1f
aberimen <berimen63@gmail.com>
2021-06-10 14:43:39 +0300

    Add React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import HomePage from './pages/HomePage'
 import './App.css';
+import LoginPage from './pages/LoginPage/LoginPage';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Navbar from './layouts/Navbar/Navbar';
+
 
 function App() {
 
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login" component={LoginPage} />
+
+        <div>
+          <Navbar />
+          <Route exact path="/" component={HomePage} />
+          <Redirect to="/" />
+        </div>
+
+      </Switch>
+    </BrowserRouter>
   );
 }
 
