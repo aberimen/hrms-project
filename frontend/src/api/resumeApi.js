2dff677c54787b62e79a72afd25913190b4b91b8
aberimen <berimen63@gmail.com>
2021-06-19 14:40:14 +0300

    Fix

--- frontend/src/api/resumeApi.js
+++ frontend/src/api/resumeApi.js
@@ -10,27 +10,27 @@ export const addEducationDetails = (educationDetails, resumeId) => {
 };
 
 export const addExperience = (experience, resumeId) => {
-    return axios.post(`/api/1.0/resumes/experiences?resumeId= ${resumeId}`, experience);
+    return axios.post(`/api/1.0/resumes/experiences?resumeId=${resumeId}`, experience);
 };
 
 export const addLanguageSkill = (languageSkill, resumeId) => {
-    return axios.post(`/api/1.0/resumes/language-skills?resumeId= ${resumeId}`, languageSkill);
+    return axios.post(`/api/1.0/resumes/language-skills?resumeId=${resumeId}`, languageSkill);
 };
 
 export const addTechnicalSkill = (technicalSkill, resumeId) => {
-    return axios.post(`/api/1.0/resumes/technical-skills?resumeId= ${resumeId}`, technicalSkill);
+    return axios.post(`/api/1.0/resumes/technical-skills?resumeId=${resumeId}`, technicalSkill);
 };
 
 export const addProfileImage = (file, resumeId) => {
-    return axios.post(`/api/1.0/resumes/profile-image?resumeId= ${resumeId}`, file);
+    return axios.post(`/api/1.0/resumes/profile-image?resumeId=${resumeId}`, file);
 };
 
 export const addSummary = (summary, resumeId) => {
-    return axios.post(`/api/1.0/resumes/summary?resumeId= ${resumeId}`, summary);
+    return axios.post(`/api/1.0/resumes/summary?resumeId=${resumeId}`, summary);
 };
 
 export const addSocialAccounts = (socialAccounts, resumeId) => {
-    return axios.post(`/api/1.0/resumes/social-accounts?resumeId= ${resumeId}`, socialAccounts);
+    return axios.post(`/api/1.0/resumes/social-accounts?resumeId=${resumeId}`, socialAccounts);
 };
 
 export const getEducationLevels = () => {
