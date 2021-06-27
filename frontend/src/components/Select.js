0c3b2d9ba151cd935e6bdaecc6ea2412b355cf59
aberimen <berimen63@gmail.com>
2021-06-28 01:16:56 +0300

    Add defaultOption

--- frontend/src/components/Select.js
+++ frontend/src/components/Select.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Select = ({ id, name, defaultValue, value, label, options = [], error, onChange, onBlur }) => {
+const Select = ({ id, name, defaultOption = {}, value, label, options = [], error, onChange, onBlur }) => {
 
     let className = "form-control form-select";
 
@@ -10,16 +10,22 @@ const Select = ({ id, name, defaultValue, value, label, options = [], error, onC
 
     return (
         <div className="form-group my-2">
+
             {label && (<label className="form-label" htmlFor={id}>{label}</label>)}
-            <select className={className} value={value} name={name} onChange={onChange} onBlur={onBlur}>
-                {defaultValue ? <option value={defaultValue} label={defaultValue} />
-                    : <option value="" label="Bir seçim yapınız" />}
+
+            <select className={className} value={value} name={name} onChange={onChange} onBlur={onBlur} >
+
+                {defaultOption ? <option value={defaultOption.value} label={defaultOption.label} />
+                    :
+                    <option value="" label="Bir seçim yapınız" />
+                }
 
                 {options.map(option =>
                     <option key={option.value} value={option.value} label={option.label} />
                 )}
 
             </select>
+
             {error && <div className="invalid-feedback">{error}</div>}
         </div>
     );
