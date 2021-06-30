7930c0a4ad4aab83ee0839b0bad46dbfb3ca9389
aberimen <berimen63@gmail.com>
2021-06-30 14:35:35 +0300

    Add constructor

--- hrms/src/main/java/com/aberimen/hrms/resume/dto/SummaryDTO.java
+++ hrms/src/main/java/com/aberimen/hrms/resume/dto/SummaryDTO.java
@@ -1,8 +1,12 @@
 package com.aberimen.hrms.resume.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SummaryDTO {
 	
 	private String summary;
