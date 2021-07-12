8e9062bfc6693cdab0084496342cded07d27e6d2
aberimen <berimen63@gmail.com>
2021-07-13 00:57:30 +0300

    Add AuthEntryPoint for customizing forbidden error message

--- hrms/src/main/java/com/aberimen/hrms/security/SecurityConfig.java
+++ hrms/src/main/java/com/aberimen/hrms/security/SecurityConfig.java
@@ -12,10 +12,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		
+		http.exceptionHandling().authenticationEntryPoint(new AuthEntryPoint()); // custom message for forbidden error
+		
 		http.authorizeRequests().antMatchers(HttpMethod.POST,"/api/1.0/job-postings").authenticated()
 		.and()
 		.authorizeRequests().anyRequest().permitAll();
 		
 	}
+	
+	
 
 }
