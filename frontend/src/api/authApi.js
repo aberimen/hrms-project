c8fd3aeda5c7cbdc489f200ef89d58bf830a30d4
aberimen <berimen63@gmail.com>
2021-07-16 17:34:17 +0300

    Add token to Authorization header

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -3,3 +3,11 @@ import axios from "axios";
 export const login = credentials => {
     return axios.post('/api/1.0/auth/login', credentials);
 };
+
+export const setAuthorizationToken = token => {
+    if(token){
+        axios.defaults.headers['Authorization'] = `Bearer ${token}`;
+    }else{
+        delete axios.defaults.headers['Authorization'];
+    }
+};
--- frontend/src/redux/actions/authActions.js
+++ frontend/src/redux/actions/authActions.js
@@ -1,4 +1,4 @@
-import { login } from '../../api/authApi';
+import { login, setAuthorizationToken } from '../../api/authApi';
 import { LOGIN_SUCCESS } from '../constants/UserActionTypes';
 
 export const loginSuccess = (user) => {
@@ -12,6 +12,8 @@ export const loginHandler = credentials => {
     return async dispatch => {
         const result = await login(credentials);
         await dispatch(loginSuccess(result.data));
+        setAuthorizationToken(result.data.token);
+        console.log(result.data.token);
         return result;
     }
 };
