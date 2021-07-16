86c0832b075d1db8d32493b6d16ab310e0890e7f
aberimen <berimen63@gmail.com>
2021-07-16 17:28:25 +0300

    Add annotation to enable method level security and make swagger public

--- hrms/src/main/java/com/aberimen/hrms/security/SecurityConfig.java
+++ hrms/src/main/java/com/aberimen/hrms/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -28,13 +30,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 
-		http.exceptionHandling().authenticationEntryPoint(new AuthEntryPoint()); // kendi hata mesajımızı dönmek için
+		http.exceptionHandling().authenticationEntryPoint(new AuthEntryPoint()); // custom forbidden error message
 
 		http.authorizeRequests()
-				//herkese açık endpointler
+				//public endpoints 
+				.antMatchers("/v2/api-docs", "/swagger-ui/**","/swagger-resources/**").permitAll()
 				.antMatchers(HttpMethod.POST, "/api/1.0/auth/login").permitAll()
 				.antMatchers(HttpMethod.GET, "/api/1.0/job-postings").permitAll()
-				//geri kalan tüm endpointler gizli olacak
+				//private endpoints
 				.anyRequest().authenticated();
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
